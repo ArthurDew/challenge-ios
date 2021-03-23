4c60e2e158f78a2146bdccb7d3214cae2d1c186c
Arthur <a.dewurstemberger@leftforwork.com>
2021-03-23 12:53:31 +0100

    banks logo display

--- challenge-ios/View/HomeCell.swift
+++ challenge-ios/View/HomeCell.swift
@@ -10,13 +10,16 @@ import UIKit
 class HomeCell : UITableViewCell {
     
     var nameLabel = UILabel()
+    let logoPicture = UIImageView()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .white
         addSubview(nameLabel)
+        addSubview(logoPicture)
         
         configNameLabel()
+        configPicture()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,6 +28,16 @@ class HomeCell : UITableViewCell {
     
     func set(bank: Bank) {
         nameLabel.text = getFlag(from: bank.country_code) + " " + bank.name
+        
+        if bank.logo_url != "" {
+        let url = URL(string: bank.logo_url)
+
+        if let data = try? Foundation.Data(contentsOf: url!) {
+            logoPicture.image = UIImage(data: data)!
+        }
+        } else {
+            logoPicture.image = #imageLiteral(resourceName: "default_logo")
+        }
     }
     
     func configNameLabel() {
@@ -34,6 +47,16 @@ class HomeCell : UITableViewCell {
         nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: logoPicture.leadingAnchor, constant: -10).isActive = true
+    }
+    
+    func configPicture() {
+        logoPicture.contentMode = .scaleAspectFit
+        logoPicture.translatesAutoresizingMaskIntoConstraints = false
+        logoPicture.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        logoPicture.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        logoPicture.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        logoPicture.widthAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
     func getFlag(from country: String) -> String {
